94a2d8e3035d20b38529ebea5ac2315960ab13b7
at47356 <101987474+at47356@users.noreply.github.com>
2023-04-23 14:53:14 +0100

    Finish Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './Header.jsx'
+import Card from './Card.jsx'
+import data from './data.jsx'
 
 function App() {
+  const generate = data.map(place => {
+    return <Card 
+      key={place.id}
+      {...place}
+      />
+      
+  }) 
 
   return (
     <div className="App">
       <Header/>
+      {generate}
+      {/* <Card/> */}
     </div>
   )
 }
---
+++ src/Card.jsx
@@ -0,0 +1,17 @@
+export default function Card (props){
+    return(
+       <div className="card-div">
+         <img src={`${props.img}`} className="main-img" />
+         <div className="info-div">
+            <div className="location-div">
+                <img src="./locationicon.svg" alt="" className="location-icon" />
+                <p className="location">{`${props.location}`}</p>
+                <a href={`${props.locationlink}`} className="location-link">View on Google Maps</a>
+            </div>
+            <p className="card-title">{`${props.title}`}</p>
+            <p className="card-date">{`${props.date}`}</p>
+            <p className="card-description">{`${props.description}`}</p>
+         </div>
+       </div>
+    )
+} 
